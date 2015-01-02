bf314265d9472e30d0397eff16bcc3baa3844e6d
Yoav <yoav@gingerlime.com>
2015-01-02 07:33:47 +0100

    bump up monit version to 5.11

--- fabfile.py
+++ fabfile.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 """
 
-monit-graphite is a fabric script to install latest monit (5.5) on debian hosts 
+monit-graphite is a fabric script to install latest monit (5.11) on debian hosts
 
 To execute:
 
     * Make sure you have fabric installed on your local host (e.g. pip install fabric)
-    * run `fab monit_install -H root@{hostname}` 
+    * run `fab monit_install -H root@{hostname}`
       (hostname should be the name of a virtual server you're installing onto)
 
 It might prompt you for the root password on the host you are trying to instal onto.
 
-Best to execute this on a clean virtual machine running Debian 6 (Squeeze). 
+Best to execute this on a clean virtual machine running Debian 6 (Squeeze).
 
 """
 
@@ -22,26 +22,26 @@ def _check_sudo():
         result = sudo('pwd')
         if result.failed:
             print "Trying to install sudo. Must be root"
-            run('apt-get update && apt-get install -y sudo')  
+            run('apt-get update && apt-get install -y sudo')
 
 def monit_install(force=False):
     """
-    Installs monit 5.5
+    Installs monit 5.11
     """
     _check_sudo()
     with settings(warn_only=True):
         result = sudo('monit -V')
-        if result.succeeded and "Monit version 5.5" in result and not force:
-            print "Monit 5.5 already installed. Skipping."
+        if result.succeeded and "Monit version 5.11" in result and not force:
+            print "Monit 5.11 already installed. Skipping."
             print "(use force=True to force install)"
             return
     sudo('apt-get update && apt-get upgrade -y')
     sudo('apt-get install -y build-essential libpam-dev libssl-dev make')
     sudo('mkdir -p /usr/local/src')
     with cd('/usr/local/src'):
-        sudo('wget -N http://mmonit.com/monit/dist/monit-5.5.tar.gz')
-        sudo('tar -zxvf monit-5.5.tar.gz')
-    with cd('/usr/local/src/monit-5.5'):
+        sudo('wget -N http://mmonit.com/monit/dist/monit-5.11.tar.gz')
+        sudo('tar -zxvf monit-5.11.tar.gz')
+    with cd('/usr/local/src/monit-5.11'):
         with settings(warn_only=True):
             result = sudo('./configure --sysconfdir=/etc/monit')
             if result.failed:
